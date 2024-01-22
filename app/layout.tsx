df9a8d6f16102e361933f41681e217497c8d80df
zwergdev <zwergdev@gmail.com>
2024-01-22 18:27:50 +0100

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Footer } from './_components/footer'
 import { Web3Provider } from './_providers/web3'
 import { Toaster } from '@/components/ui/sonner'
 import { WhaleBg } from '@/components/ui/icons'
+import { Analytics } from '@vercel/analytics/react'
 
 const poppins = Poppins({
   weight: ['100', '300', '400', '500', '600', '700', '800', '900'],
@@ -60,6 +61,7 @@ export default function RootLayout({
           </div>
         </Web3Provider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
