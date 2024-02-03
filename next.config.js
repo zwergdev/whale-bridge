b7871f6c71df74752e2764376b1766ddd45d55f5
zwergdev <zwergdev@gmail.com>
2024-02-03 01:06:49 +0100

    fix: deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { ppr: true },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
