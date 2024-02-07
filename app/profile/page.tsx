e68607ee6fe5ec85b7ef0925f1aaeaef4a04bb8b
zwergdev <zwergdev@gmail.com>
2024-02-07 11:04:40 +0100

    feat: add muted leaderboard

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -36,7 +36,11 @@ export default function ProfilePage() {
   if (isLoading) return <Loader className="mb-4 h-8 w-8 animate-spin-slow" />
 
   return (
-    <section className="flex max-w-screen-xl mx-auto w-full flex-col items-center justify-center gap-5 pt-16">
+    <section className="flex max-w-screen-xl mx-auto w-full flex-col items-center justify-center gap-5 pt-8">
+      <h6 className="font-medium text-xl text-muted-foreground mb-8 cursor-not-allowed">
+        Leaderboard
+      </h6>
+
       <h6 className="py-2 px-6 rounded-xl sm:text-sm text-xs bg-popover">
         <span className="sm:inline-flex hidden">Address:</span> {address}
       </h6>
