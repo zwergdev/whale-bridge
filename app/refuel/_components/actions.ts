852b2e4accb5b1f91e75d181e0145357a336a5ca
HzDev3628 <artem.khar5uk@gmail.com>
2024-02-10 19:29:09 +0200

    minor

--- app/refuel/_components/actions.ts
+++ app/refuel/_components/actions.ts
@@ -20,18 +20,44 @@ export const fetchPrices = async (): Promise<Prices> => {
   if (res.ok) return await res.json()
 
   return {
-    'avalanche-2': { usd: 39.06 },
-    binancecoin: { usd: 321.23 },
-    celo: { usd: 0.713664 },
-    ethereum: { usd: 2485.29 },
-    fantom: { usd: 0.391561 },
-    harmony: { usd: 0.01528527 },
-    kava: { usd: 0.720289 },
-    'klay-token': { usd: 0.217367 },
-    'matic-network': { usd: 0.842662 },
-    meter: { usd: 2.72 },
-    moonbeam: { usd: 0.382244 },
-    moonriver: { usd: 22.71 },
-    xdai: { usd: 1.007 },
+    'avalanche-2': {
+      usd: 39.06,
+    },
+    binancecoin: {
+      usd: 321.23,
+    },
+    celo: {
+      usd: 0.713664,
+    },
+    ethereum: {
+      usd: 2485.29,
+    },
+    fantom: {
+      usd: 0.391561,
+    },
+    harmony: {
+      usd: 0.01528527,
+    },
+    kava: {
+      usd: 0.720289,
+    },
+    'klay-token': {
+      usd: 0.217367,
+    },
+    'matic-network': {
+      usd: 0.842662,
+    },
+    meter: {
+      usd: 2.72,
+    },
+    moonbeam: {
+      usd: 0.382244,
+    },
+    moonriver: {
+      usd: 22.71,
+    },
+    xdai: {
+      usd: 1.007,
+    },
   }
 }
